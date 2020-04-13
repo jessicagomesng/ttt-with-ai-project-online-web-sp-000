4d17acefe3279e12c9a5b7403e38d3d06d8ce85b
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-13 18:54:58 +0000

    Automatically backed up by Learn

--- lib/players/minimax.rb
+++ lib/players/minimax.rb
@@ -68,7 +68,7 @@ class Players
           board.cells[move] = ""
           if score[0] < bestScore
             bestScore = score[0]
-            bestMove = move
+            bestMove = move + 1.to_s
           end
         end
         return bestMove
